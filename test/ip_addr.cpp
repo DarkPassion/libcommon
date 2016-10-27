a5ef6de44447d22f27f46a681664a1a1e853f9ef
zhangzhifan <zhangzhifan@myhug.cn>
2016-10-27 22:53:04 +0800

    ip addr test file add

--- test/ip_addr.cpp
+++ test/ip_addr.cpp
@@ -10,6 +10,7 @@
 #include <stdio.h>
 
 void test_ip_addr();
+void test_ip_addr2();
 
 void test_ip_addr()
 {
@@ -19,48 +20,48 @@ void test_ip_addr()
     if (t.get_ip_addr(buff) == 0) {
 
         printf("got ip address %s \n", buff);
-        
+
         int s = socket(AF_INET, SOCK_STREAM, 0);
         if (s < 0) {
             printf("got error socket ! \n");
             return ;
         }
-        
+
         struct sockaddr_in addr;
         memset(&addr, 0, sizeof(addr));
         addr.sin_family = AF_INET;
         addr.sin_port = htons(80);
         addr.sin_addr.s_addr = inet_addr(buff);
-        
+
         if (connect(s, (struct sockaddr*)&addr, sizeof(sockaddr_in)) < 0) {
             printf("got error socket connect! [%s]\n", strerror(errno));
             close(s);
             return ;
         }
-        
+
         printf("got succ connect !! \n");
-        
+
         close(s);
     }
-    
+
     memset(buff, 0, sizeof(buff));
     if (t.get_ip_addr(AF_INET, buff) == 0) {
-        
+
         printf("got ip v4 address %s \n", buff);
     }
-    
+
     memset(buff, 0, sizeof(buff));
     if (t.get_ip_addr(AF_INET6, buff) == 0) {
-    
+
         printf("got ip v6 address %s \n", buff);
     }
-    
+
     memset(buff, 0, sizeof(buff));
     struct sockaddr sa = {0};
     int sa_len = 0;
     if (t.get_ip_addr2(AF_INET, 80, &sa, sa_len) == 0) {
         printf("got ip address 2 success !\n");
-        
+
         int s  = socket(AF_INET, SOCK_STREAM, 0);
         if (s < 0) {
             printf("got error socket ! \n");
@@ -71,31 +72,93 @@ void test_ip_addr()
             close(s);
             return ;
         }
-        
+
         printf("got socket connect success! \n");
         close(s);
     }
-    
+
     typedef std::vector<std::string> TypeIpList;
     TypeIpList ip_list;
     if (t.get_ip_addr(ip_list) == 0) {
         printf("got ip address list success! \n");
-        
+
         TypeIpList::iterator iter = ip_list.begin();
         while (iter != ip_list.end()) {
             printf("ip list [%s] \n", iter->c_str());
             iter++;
         }
-        
+
     }
-   
+
 }
 
+void test_ip_bind_func(struct sockaddr* addr, int len)
+{
+    int s = socket(AF_INET, SOCK_STREAM, 0);
+    if (s < 0) {
+        printf("got socket error ! \n");
+        return ;
+    }
+
+    int option = 1;
+    if (setsockopt (s, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option)  ) < 0) {
+        printf("set socket opt error! SO_REUSEADDR \n");
+    }
+
+    if (bind(s, addr, len) == 0) {
+        printf("bind succ ! \n");
+    } else {
+        printf("%s == bind fail ! \n", __FUNCTION__);
+    }
+
+    close(s);
+}
+
+void test_ip_addr2()
+{
+
+    if (true) {
+        int len = 0;
+        struct sockaddr addr = {0};
+        bind_addr_t t("127.0.0.1", 8080);
+        if (t.get_bind_addr(AF_INET, &addr, len) == 0) {
+            printf("got bind addr success 127.0.0.1 !! \n");
+            test_ip_bind_func(&addr, len);
+        } else {
+            printf("got bind addr error! \n");
+        }
+    }
+
+    if (true) {
+        int len = 0;
+        struct sockaddr addr = {0};
+        bind_addr_t t("128.1.0.1", 8080);
+        if (t.get_bind_addr(AF_INET, &addr, len) == 0) {
+            printf("got bind addr success localhost !! \n");
+            test_ip_bind_func(&addr, len);
+        } else {
+            printf("got bind addr error! \n");
+        }
+    }
+
+    if (true) {
+        int len = 0;
+        struct sockaddr addr = {0};
+        bind_addr_t t("0.0.0.0", 8080);
+        if (t.get_bind_addr(AF_INET, &addr, len) == 0) {
+            printf("got bind addr success 0.0.0.0 !! \n");
+            test_ip_bind_func(&addr, len);
+        } else {
+            printf("got bind addr error! \n");
+        }
+    }
+
+}
 
 int main()
 {
     test_ip_addr();
-
+    test_ip_addr2();
     return 0;
 }
 
