c002d1ece831136129d89b8b74ea53f37d9b302a
zhangzhifan <zhangzhifan@baidu.com>
2017-09-09 13:55:49 +0800

    update scoped_ptr

--- include/util/util.h
+++ include/util/util.h
@@ -3,6 +3,8 @@
 #define _UTIL_UTIL_H_
 
 #include <stdlib.h>  // NULL
+#include <cstddef>             // for std::ptrdiff_t
+#include <assert.h>            // for assert
 
 namespace libcommon
 {
@@ -51,80 +53,238 @@ DISALLOW_EVIL_CONSTRUCTORS(TypeName)
         pa = NULL; \
     }
 
-template <class T>
-class scope_ptr
-{
-private:
-    T** _ptr;
-
-    DISALLOW_COPY_AND_ASSIGN(scope_ptr);
-public:
-    scope_ptr(T** ptr)
-    {
-        _ptr = ptr;
-    }
-
-    ~scope_ptr()
-    {
-        if (_ptr && *_ptr)
-        {
-            delete *_ptr;
-            *_ptr = NULL;
+    
+    // ----------------------------------
+    // class scoped_ptr
+    // ----------------------------------
+    
+    template <typename T>
+    class scoped_ptr {
+    private:
+        
+        T* ptr;
+        
+        DISALLOW_COPY_AND_ASSIGN(scoped_ptr);
+        
+    public:
+        
+        typedef T element_type;
+        
+        explicit scoped_ptr(T* p = NULL): ptr(p) {}
+        
+        ~scoped_ptr() {
+            typedef char type_must_be_complete[sizeof(T)];
+            delete ptr;
         }
-    }
-};
-
-template <class T>
-class scope_ptr<T[]>
-{
-private:
-    T** _ptr;
-    DISALLOW_COPY_AND_ASSIGN(scope_ptr);
-
-public:
-    scope_ptr(T** ptr)
-    {
-        _ptr = ptr;
-    }
-
-    ~scope_ptr()
-    {
-        if (_ptr && *_ptr)
-        {
-            delete[] *_ptr;
-            *_ptr = NULL;
+        
+        void reset(T* p = NULL) {
+            typedef char type_must_be_complete[sizeof(T)];
+            
+            if (ptr != p) {
+                T* obj = ptr;
+                ptr = p;
+                // Delete last, in case obj destructor indirectly results in ~scoped_ptr
+                delete obj;
+            }
         }
-    }
-};
-
-#define AutoDeleteScopePtr(cls, val) scope_ptr<cls> scope_ptr##val(&val)
-
-
-template <class T>
-class scope_ptr_c
-{
-private:
-    void**  _ptr;
-    DISALLOW_COPY_AND_ASSIGN(scope_ptr_c);
-
-public:
-    scope_ptr_c(T** ptr)
-    {
-        _ptr = (void**)ptr;
-    }
-
-    ~scope_ptr_c()
-    {
-        if (_ptr && *_ptr)
-        {
-            free(*_ptr);
-            *_ptr = NULL;
+        
+        T& operator*() const {
+            assert(ptr != NULL);
+            return *ptr;
         }
+        
+        T* operator->() const  {
+            assert(ptr != NULL);
+            return ptr;
+        }
+        
+        T* get() const  {
+            return ptr;
+        }
+        
+        void swap(scoped_ptr & b) {
+            T* tmp = b.ptr;
+            b.ptr = ptr;
+            ptr = tmp;
+        }
+        
+        T* release() {
+            T* tmp = ptr;
+            ptr = NULL;
+            return tmp;
+        }
+        
+        T** accept() {
+            if (ptr) {
+                delete ptr;
+                ptr = NULL;
+            }
+            return &ptr;
+        }
+        
+        T** use() {
+            return &ptr;
+        }
+    };
+    
+    template<typename T> inline
+    void swap(scoped_ptr<T>& a, scoped_ptr<T>& b) {
+        a.swap(b);
     }
-};
 
-#define AutoFreeScopePtr(cls, val) scope_ptr_c<cls> scope_ptr_c##val(&val)
+    
+    
+    // ----------------------------------
+    // class scoped_array
+    // ----------------------------------
+    
+    //  scoped_array extends scoped_ptr to arrays. Deletion of the array pointed to
+    //  is guaranteed, either on destruction of the scoped_array or via an explicit
+    //  reset(). Use shared_array or std::vector if your needs are more complex.
+    
+    template<typename T>
+    class scoped_array {
+    private:
+        
+        T* ptr;
+        
+        scoped_array(scoped_array const &);
+        scoped_array & operator=(scoped_array const &);
+        
+    public:
+        
+        typedef T element_type;
+        
+        explicit scoped_array(T* p = NULL) : ptr(p) {}
+        
+        ~scoped_array() {
+            typedef char type_must_be_complete[sizeof(T)];
+            delete[] ptr;
+        }
+        
+        void reset(T* p = NULL) {
+            typedef char type_must_be_complete[sizeof(T)];
+            
+            if (ptr != p) {
+                T* arr = ptr;
+                ptr = p;
+                // Delete last, in case arr destructor indirectly results in ~scoped_array
+                delete [] arr;
+            }
+        }
+        
+        T& operator[](std::ptrdiff_t i) const {
+            assert(ptr != NULL);
+            assert(i >= 0);
+            return ptr[i];
+        }
+        
+        T* get() const {
+            return ptr;
+        }
+        
+        void swap(scoped_array & b) {
+            T* tmp = b.ptr;
+            b.ptr = ptr;
+            ptr = tmp;
+        }
+        
+        T* release() {
+            T* tmp = ptr;
+            ptr = NULL;
+            return tmp;
+        }
+        
+        T** accept() {
+            if (ptr) {
+                delete [] ptr;
+                ptr = NULL;
+            }
+            return &ptr;
+        }
+    };
+    
+    template<class T> inline
+    void swap(scoped_array<T>& a, scoped_array<T>& b) {
+        a.swap(b);
+    }
+    
+    
+    
+    
+    // ----------------------------------
+    // class scoped_ptr_malloc
+    // ----------------------------------
 
+    // scoped_ptr_malloc<> is similar to scoped_ptr<>, but it accepts a
+    // second template argument, the function used to free the object.
+    
+    template<typename T, void (*FF)(void*) = free> class scoped_ptr_malloc {
+    private:
+        
+        T* ptr;
+        
+        scoped_ptr_malloc(scoped_ptr_malloc const &);
+        scoped_ptr_malloc & operator=(scoped_ptr_malloc const &);
+        
+    public:
+        
+        typedef T element_type;
+        
+        explicit scoped_ptr_malloc(T* p = 0): ptr(p) {}
+        
+        ~scoped_ptr_malloc() {
+            FF(static_cast<void*>(ptr));
+        }
+        
+        void reset(T* p = 0) {
+            if (ptr != p) {
+                FF(static_cast<void*>(ptr));
+                ptr = p;
+            }
+        }
+        
+        T& operator*() const {
+            assert(ptr != 0);
+            return *ptr;
+        }
+        
+        T* operator->() const {
+            assert(ptr != 0);
+            return ptr;
+        }
+        
+        T* get() const {
+            return ptr;
+        }
+        
+        void swap(scoped_ptr_malloc & b) {
+            T* tmp = b.ptr;
+            b.ptr = ptr;
+            ptr = tmp;
+        }
+        
+        T* release() {
+            T* tmp = ptr;
+            ptr = 0;
+            return tmp;
+        }
+        
+        T** accept() {
+            if (ptr) {
+                FF(static_cast<void*>(ptr));
+                ptr = 0;
+            }
+            return &ptr;
+        }
+    };
+    
+    template<typename T, void (*FF)(void*)> inline
+    void swap(scoped_ptr_malloc<T,FF>& a, scoped_ptr_malloc<T,FF>& b) {
+        a.swap(b);
+    }
+    
+    
 
 }
 #endif
--- test/util.cpp
+++ test/util.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "util/util.h"
 
 using namespace libcommon;
@@ -7,9 +8,9 @@ using namespace libcommon;
 // g++ util.cpp -I../include/ -L../libs/ -lcommon -o util.out
 // valgrind --tool=memcheck --leak-check=full ./util.out
 
-void test_util();
-
-
+void test_util_scoped_ptr();
+void test_util_scoped_array_ptr();
+void test_util_scoped_ptr_malloc();
 
 struct obj0_t
 {
@@ -18,44 +19,84 @@ struct obj0_t
 	int seq;
 };
 
-void test_util()
+
+
+void func_obj0_t(obj0_t* obj)
 {
-	char* tmp = new char[100];
-	AutoDeleteScopePtr(char, tmp);
+    if (obj) {
+        printf("-------- obj->id  = %d \n", obj->id);
+        printf("-------- obj->inc  = %d \n", obj->inc);
+        printf("-------- obj->seq = %d \n", obj->seq);
+    } else {
+        printf("-------- obj == null \n");
+    }
+}
 
-	char* tmp1 = new char;
-	AutoDeleteScopePtr(char, tmp1);
+void test_util_scoped_ptr()
+{
+    scoped_ptr<char> tmp;
+    
+    scoped_ptr<obj0_t> obj1;
+    obj1.reset(new obj0_t());
+    if (obj1.get()) {
+        obj1->id = 100;
+        obj1->inc = 100;
+        obj1->seq = 100;
+    }
+    
+    func_obj0_t(obj1.get());
+    obj1.reset();
+    func_obj0_t(obj1.get());
+}
 
-	obj0_t * obj0 = new obj0_t;
-	AutoFreeScopePtr(obj0_t, obj0);
+void test_util_scoped_array_ptr()
+{
+    scoped_array<char> obj1;
+    char* pt = new char[100];
+    obj1.reset(pt);
+    if (obj1.get() != pt) {
+        printf("error ------ scoped_array get not equal ! \n");
+    }
 
-	if (obj0)
-	{
-		/* code */
-		obj0->id = 100;
-		obj0->inc = 101;
-		obj0->seq = 11;
-	}
-
-	printf("[%d %d %d]\n",obj0->id, obj0->inc, obj0->seq);
-	/*
-		some process with obj0
-	 */
-
-	obj0_t* obja = new obj0_t[10];
-	AutoDeleteScopePtr(obj0_t[], obja);
+    char h[] = "hello world";
+    memcpy(pt, h, sizeof(h));
+    
+    printf(" ------ %s \n", h);
+    printf(" ------ %s \n", obj1.get());
+    if (strcmp(h, obj1.get()) != 0) {
+        printf("error ------ scoped_array get not equal ! \n");
+    }
+    
+}
 
 
-	char* aa1 = (char*)malloc(100);
-	AutoFreeScopePtr(char, aa1);
+void test_util_scoped_ptr_malloc()
+{
+    scoped_ptr_malloc<char> obj1;
+    if (obj1.get() != NULL) {
+        printf("error ----- scoped_ptr_malloc get not null ! \n");
+    }
+    char* pt = (char*)malloc(100);
+    char h[] = "hello world";
+    memcpy(pt, h, sizeof(h));
+    obj1.reset(pt);
+    if (obj1.get() != pt) {
+        printf("error ------- scoped_ptr_malloc get ! \n");
+    }
+    
+    if (strcmp(pt, obj1.get()) != 0) {
+        printf("error ------ scoped_ptr_malloc strcmp not equal \n");
+    }
 
 }
 
 
 int main()
 {
-	test_util();
-
+    test_util_scoped_ptr();
+    test_util_scoped_array_ptr();
+    test_util_scoped_ptr_malloc();
+    
 	return 0;
 }
 
