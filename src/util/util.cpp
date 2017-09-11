08a41ff8013943628c6f03851351eba0de9105ef
zhangzhifan <zhangzhifan@baidu.com>
2017-09-11 18:16:46 +0800

    udpate singleton.h

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -1,3 +1,18 @@
 #include "util/util.h"
+#include "util/ref_count.h"
 
-
+namespace libcommon {
+    
+    
+    void GoogleOnceInit(int* once, void(*init_func)(void))
+    {
+        
+        // __sync_bool_compare_and_swap(ptr, oval, nval)
+        if (ATOM_CAS(once, ONCE_STATE_UNINITIALIZED, ONCE_STATE_DONE)) {
+            if (init_func) {
+                init_func();
+            }
+        }
+    }
+    
+}
